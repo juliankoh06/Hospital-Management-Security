bbcb5227d8d7d50c50b0e3fc3cc289a5b9cffe3c
yong23015105 <135498630+yong23015105@users.noreply.github.com>
2025-11-23 00:03:22 +0800

    Update messearch.php

--- messearch.php
+++ messearch.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
- <?php #include("func.php");?>
+<?php #include("func.php");?>
 <html>
 <head>
   <title>User Messages</title>
@@ -7,11 +6,18 @@
 </head>
 <body>
 <?php
+// --- CRITICAL FIX: Added connection file ---
+include("newfunc.php");
+// -------------------------------------------
+
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 if(isset($_POST['mes_search_submit']))
 {
   try {
       $contact=$_POST['mes_contact'];
+      
+      // Secure Prepared Statement
       $query = "select * from contact where contact= ?";
       $stmt = mysqli_prepare($con, $query);
       mysqli_stmt_bind_param($stmt, "s", $contact);
@@ -19,8 +25,8 @@ if(isset($_POST['mes_search_submit']))
       $result=mysqli_stmt_get_result($stmt);
       $row=mysqli_fetch_array($result);
       
-      // check if $row exists first to avoid errors if no result found
-      if(!$row || ($row['name']=="" & $row['email']=="" & $row['contact']=="" & $row['message']=="")){
+      // Check if row exists first, then check for empty fields
+      if(!$row || ($row['name']=="" && $row['email']=="" && $row['contact']=="" && $row['message']=="")){
         echo "<script> alert('No entries found! Please enter valid details'); 
               window.location.href = 'admin-panel1.php#list-doc';</script>";
       } 
@@ -39,17 +45,17 @@ if(isset($_POST['mes_search_submit']))
         </thead>
         <tbody>";
       
-        
-              $name = htmlspecialchars($row['name']);
-              $email = htmlspecialchars($row['email']);
-              $contact = htmlspecialchars($row['contact']);
-              $message = htmlspecialchars($row['message']);
-              echo "<tr>
-                <td>$name</td>
-                <td>$email</td>
-                <td>$contact</td>
-                <td>$message</td>
-              </tr>";
+            $name = htmlspecialchars($row['name']);
+            $email = htmlspecialchars($row['email']);
+            $contact = htmlspecialchars($row['contact']);
+            $message = htmlspecialchars($row['message']);
+            
+            echo "<tr>
+              <td>$name</td>
+              <td>$email</td>
+              <td>$contact</td>
+              <td>$message</td>
+            </tr>";
         
         echo "</tbody></table><center><a href='admin-panel1.php' class='btn btn-light'>Back to your Dashboard</a></div></center></div></div></div>";
       }
@@ -62,6 +68,5 @@ if(isset($_POST['mes_search_submit']))
 ?>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script> 
 </body>
 </html>
